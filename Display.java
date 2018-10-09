a4dd29dc9a5e57aa8589a0b9ee081cff9a8fc9b6
LasaBir <stanleymines@gmail.com>
2018-10-08 19:42:00 -0700

    Original Work

---
+++ Display.java
@@ -0,0 +1,296 @@
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.awt.*;
+import java.nio.DoubleBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Display
+{
+  private static long window;
+  public static WindowSize w;
+  private static Mandelbrot mandelbrot;
+
+  public Display()
+  {
+    mandelbrot = new Mandelbrot();
+  }
+
+  public void run()
+  {
+    init();
+    loop();
+    terminate();
+  }
+
+  private void init()
+  {
+    // Prevent java.awt from interfering with this thread. If any awt stuff is called prior to this, the program will crash (Ex: {@code Color c = new Color(255,0,255);}).
+    System.setProperty("java.awt.headless", "true");
+
+    // Setup an error callback. The default implementation
+    // will print the error message in System.err.
+    GLFWErrorCallback.createPrint(System.err).set();
+
+    // Initialize GLFW. Most GLFW functions will not work before doing this.
+    if (!glfwInit())
+      throw new IllegalStateException("Unable to initialize GLFW");
+
+    // Configure GLFW
+    glfwDefaultWindowHints(); // optional, the current window hints are already the default
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // whether the window can be resized.
+
+    // Create the window
+    window = glfwCreateWindow(800, 600, "Mandelbrot Zoom", NULL, NULL);
+    if (window == NULL)
+      throw new RuntimeException("Failed to create the GLFW window");
+
+    registerCallbacks();
+
+    // Get the thread stack and push a new frame
+    try (MemoryStack stack = MemoryStack.stackPush())
+    {
+      IntBuffer pWidth = stack.mallocInt(1); // int*
+      IntBuffer pHeight = stack.mallocInt(1); // int*
+
+      // Get the window size passed to glfwCreateWindow
+      glfwGetWindowSize(window, pWidth, pHeight);
+
+      // Get the resolution of the primary monitor
+      GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+      // Center the window
+      glfwSetWindowPos(
+          window,
+          (vidmode.width() - pWidth.get(0)) / 2,
+          (vidmode.height() - pHeight.get(0)) / 2
+      );
+    } // the stack frame is popped automatically
+
+    // Make the OpenGL context current
+    glfwMakeContextCurrent(window);
+    // Enable v-sync
+    glfwSwapInterval(1);
+
+    // Make the window visible
+    glfwShowWindow(window);
+  }
+
+  /**
+   * Register the call backs from GLFW for the keyboard, mouse clicks, and scrolling.
+   */
+  private void registerCallbacks()
+  {
+    // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+    glfwSetKeyCallback(window, (window, key, scancode, action, mods) ->
+    {
+        mandelbrot.keyPress(key, action);
+
+      if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+      {
+        mandelbrot.clearThreads();
+        glfwSetWindowShouldClose(window, true);
+      }
+    });
+
+    glfwSetMouseButtonCallback(window, (window, button, action, mods) ->
+    {
+        if (action == GLFW_PRESS)
+        {
+          mandelbrot.doClick();
+        }
+    });
+  }
+
+  private void loop()
+  {
+    // This line is critical for LWJGL's interoperation with GLFW's
+    // OpenGL context, or any context that is managed externally.
+    // LWJGL detects the context that is current in the current thread,
+    // creates the GLCapabilities instance and makes the OpenGL
+    // bindings available for use.
+    GL.createCapabilities();
+
+    // Set the clear/background color
+    glClearColor(0.0f, 0.0f, 0.25f, 1.0f); // Was that greenish: .3 .7 .6 .0
+
+
+    // Run the rendering loop until the user has attempted to close
+    // the window or has pressed the ESCAPE key.
+
+    long time = System.nanoTime();
+    while (!glfwWindowShouldClose(window))
+    {
+      time = sleep(time);
+
+      glfwPollEvents();
+
+      update();
+      render();
+      finishRender();
+    }
+  }
+  private long sleep(long last_execution)
+  {
+    double maxSleepMS = 1000D / 60;
+    double msSinceLastSleep = (System.nanoTime() - last_execution) / 1000000D;
+    if (maxSleepMS > msSinceLastSleep)
+    {
+      try
+      {
+        Thread.sleep((long) (maxSleepMS - msSinceLastSleep));
+      } catch (InterruptedException e)
+      {
+        System.err.println("Sleep Interrupted!");
+        e.printStackTrace();
+      }
+    }
+     return System.nanoTime();
+  }
+
+  private void update()
+  {
+    w = getWindowSize();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glDepthFunc(GL_LESS);
+    glShadeModel(GL_SMOOTH);
+  }
+
+  private void render()
+  {
+    glPointSize(10);
+    glLineWidth(5F);
+    Display.setColor3(Color.BLUE);
+    mandelbrot.display();
+  }
+
+  /**
+   * Displays the rendered frame, and takes care of double buffering stuff.
+   */
+  private void finishRender()
+  {
+    glfwSwapBuffers(window); // swap the color buffers
+  }
+
+  /**
+   * Closes the window and does other cleanup stuff.
+   */
+  private void terminate()
+  {
+    // Free the window callbacks and destroy the window
+    glfwFreeCallbacks(window);
+    glfwDestroyWindow(window);
+
+    // Terminate GLFW and free the error callback
+    glfwTerminate();
+    glfwSetErrorCallback(null).free();
+  }
+
+  public static void doPointCart(double x, double y)
+  {
+    glVertex2d(x / (w.w / 2D), y / (w.h / 2D));
+  }
+
+  public static void doPointOr(double x, double y)
+  {
+    doPointCart(x - w.w / 2D, w.h / 2D - y);
+  }
+
+  public static void setColor3(Color c)
+  {
+    glColor3f(c.getRed() / 255f, c.getGreen() / 255f, c.getBlue() / 255f);
+  }
+  public static Color hsb4ToColor(int hue, int saturation, int brightness, int alpha)
+  {
+    if (hue < 0)
+      hue = hue % 360 + 360;
+    if (hue >= 360)
+      hue %= 360;
+    saturation = normalizeInt(saturation, 0, 100);
+    brightness = normalizeInt(brightness, 0, 100);
+
+    saturation = saturation * 255 / 100;
+    brightness = brightness * 255 / 100;
+
+    int red = 0, green = 0, blue = 0;
+
+    if (saturation == 0) { red = green = blue = brightness; }
+    else
+    {
+      int t1 = brightness;
+      int t2 = (255 - saturation) * brightness / 255;
+      int t3 = (t1 - t2) * (hue % 60) / 60;
+
+      if (hue < 60) { red = t1; blue = t2; green = t2 + t3; }
+      else if (hue < 120) { green = t1; blue = t2; red = t1 - t3; }
+      else if (hue < 180) { green = t1; red = t2; blue = t2 + t3; }
+      else if (hue < 240) { blue = t1; red = t2; green = t1 - t3; }
+      else if (hue < 300) { blue = t1; green = t2; red = t2 + t3; }
+      else if (hue < 360) { red = t1; green = t2; blue = t1 - t3; }
+      else { red = 0; green = 0; blue = 0; }
+    }
+    return new Color(red, green, blue, alpha);
+  }
+  public static WindowSize getWindowSize()
+  {
+    try (MemoryStack stack = MemoryStack.stackPush())
+    {
+      //int* width = malloc(1);
+      IntBuffer pWidth = stack.mallocInt(1); // int*
+      IntBuffer pHeight = stack.mallocInt(1); // int*
+
+      glfwGetWindowSize(window, pWidth, pHeight);
+      return new WindowSize(pWidth.get(), pHeight.get());
+    } catch (IllegalArgumentException e)
+    {
+      return new WindowSize(0, 0);
+    }
+  }
+  public static Point getCursorLocationOrigin(WindowSize w)
+  {
+    try (MemoryStack stack = MemoryStack.stackPush())
+    {
+      //double* x = malloc(1);
+      DoubleBuffer x = stack.mallocDouble(1); // int*
+      DoubleBuffer y = stack.mallocDouble(1); // int*
+
+      glfwGetCursorPos(window, x, y);
+
+      return new Point((int) Math.round(x.get()), (int) Math.round(y.get()));
+    } catch (IllegalArgumentException e)
+    {
+      return null;
+    }
+  }
+  public static int normalizeInt(int value, int min, int max)
+  {
+    if (value <= min)
+      return min;
+    if (value >= max)
+      return max;
+    return value;
+  }
+  public static class WindowSize
+  {
+    public final int w;
+    public final int h;
+    public WindowSize(int width, int height)
+    {
+      w = width;
+      h = height;
+    }
+
+    public boolean equals(WindowSize o)
+    {
+      return o!=null && w==o.w && h==o.h;
+    }
+  }
+}
---
+++ Mandelbrot.java
@@ -0,0 +1,364 @@
+import java.awt.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+public class Mandelbrot
+{
+  public static int COLORRATE = 5;
+  public static final int TILESIZE = 150;
+  public static final int MAXTHREADS = 4*8+1;
+  private volatile Integer threadCount = 0; private synchronized int getThreads(){ return threadCount;} private synchronized void addThread(){ if (threadCount >=MAXTHREADS) System.err.println("Attempting to create a thread exceeding thread limit!"); threadCount++;} private synchronized void remThread(){ threadCount--;} private synchronized boolean canStartNewThread(){return threadCount<MAXTHREADS;}
+  private volatile List<Thread> threads = Collections.synchronizedList(new ArrayList<Thread>(MAXTHREADS));
+  HashMap<HashableView, Color[]> calculated = new HashMap<>(500);
+  Display.WindowSize w;
+
+  double scale = -7.5;
+  //double scale = -20.5;
+  QP center = qp(q(-1, 2), q(0, 1));
+  //QP center = qp(q(81448, 49550959), q(-24170803,29388151));
+
+  public static void main(String[] args)
+  {
+    if (args.length>=1)
+      COLORRATE = new Integer(args[0]);
+    if (COLORRATE<1)
+      COLORRATE = 5;
+    new Display().run();
+  }
+
+  public void doClick()
+  {
+    Point pt = Display.getCursorLocationOrigin(w);
+    Q scaleFactor = q(2).pow((int) scale).m(trot((int) (scale * 10) % 10));
+    center = qp(center.x.a(q(pt.x - w.w / 2).m(scaleFactor)), center.y.s(q(w.h / 2 - pt.y).m(scaleFactor)));
+
+    clearThreads();
+    calculated.clear();
+
+    System.out.printf("%sNow centered on (%s + %s * i)%s", "\n", center.x, center.y, "\n");
+  }
+
+  public void keyPress(int key, int action)
+  {
+    if (action==GLFW_RELEASE)
+    {
+      if (key == GLFW_KEY_EQUAL || key == GLFW_KEY_KP_ADD)
+      {
+        scale += .5 * -1;
+        clearThreads();
+      }
+      if (key == GLFW_KEY_MINUS|| key == GLFW_KEY_KP_SUBTRACT)
+      {
+        scale += .5 * 1;
+        clearThreads();
+      }
+      System.out.println("The scale is now: " + scale);
+    }
+  }
+  public void display()
+  {
+    if (!Display.w.equals(this.w))
+    {
+      calculated.clear();
+      clearThreads();
+      this.w = Display.w;
+    }
+    HashableView hv = new HashableView(center, scale);
+    int hvhc = hv.hashCode();
+
+    Color[] precalculated = null;
+    for (HashableView h : calculated.keySet())
+      if (h.hashCode() == hvhc)
+        precalculated = calculated.get(h);
+
+    synchronized (threads)
+    {
+      ArrayList<Thread> toRem = new ArrayList<>();
+      for (Thread thrd : threads)
+        if (!thrd.isAlive())
+        {
+          toRem.add(thrd);
+        }
+      for (Thread thrd : toRem)
+      {
+        thrd.interrupt();
+        threads.remove(thrd);
+        remThread();
+      }
+    }
+
+    if (precalculated == null || (getThreads()==0 && cont(precalculated,null)))
+      calculate(hv);
+    else
+      display(precalculated);
+  }
+
+  public static boolean cont (Object[] data, Object value)
+  {
+    for (Object o : data)
+      if (o==value)
+        return true;
+    return false;
+  }
+
+  public void display(Color[] data)
+  {
+    glPointSize(5f);
+    for (int y = 0; y < w.h; y++)
+      for (int x = 0; x < w.w; x++)
+      {
+        Color c = data[w.w * y + x];
+        if (c != null)
+          Display.setColor3(c);
+        else
+          glColor3f(.125f, .125f, .125f);
+        glBegin(GL_POINTS);
+        Display.doPointOr(x, y);
+        glEnd();
+      }
+  }
+
+  public void calculate(HashableView hv)
+  {
+    Color[] clrset = new Color[w.w*w.h];
+    calculated.put(hv, clrset);
+    Thread runner = new Thread(()->
+    {
+      Q scaleFactor = q(2).pow((int) scale).m(trot((int) (scale * 10) % 10));
+      HashSet<int[]> tiles = new HashSet<>();
+      for (int y = -w.h/2; y < w.h/2; y+=TILESIZE)
+        for (int x = -w.w/2; x < w.w/2; x+=TILESIZE)
+        {
+          int[] toPut = new int[]{x, y, Display.normalizeInt(x + TILESIZE - 1, -w.w / 2, w.w / 2 - 1), Display.normalizeInt(y + TILESIZE - 1, -w.h / 2, w.h / 2 - 1)};
+          tiles.add(toPut);
+        }
+      Iterator<int[]> tileSet= tiles.iterator();
+      while (tileSet.hasNext())
+      {
+        if (canStartNewThread())
+        {
+          final int[] workTile = tileSet.next();
+          Thread worker = new Thread(() ->
+          {
+            for (int y = workTile[1]; y <= workTile[3]; y++)
+            {
+              Q qy = (hv.pt.y.a(scaleFactor.m(y)));
+              int yPositionOffset = (y + w.h / 2) * w.w + w.w / 2;
+              for (int x = workTile[0]; x <= workTile[2]; x++)
+              {
+                long steps = -1;
+                try {
+                  steps = countStepsSimple(new Double(hv.pt.x.a(scaleFactor.m(x)).toString()), new Double(qy.toString()), 2048);
+                } catch (Exception e)
+                {
+                  System.out.println("{"+hv.pt.x.a(scaleFactor.m(x)).toString()+","+qy.toString()+"}");
+                }
+                synchronized (clrset)
+                {
+                  if (steps == -1)
+                    clrset[yPositionOffset + x] = Color.BLACK;
+                  else
+                    clrset[yPositionOffset + x] = Display.hsb4ToColor(COLORRATE * (int) (steps % 360), 90, 90, 255);
+                }
+              }
+            }
+          });
+          synchronized (threads)
+          {
+            threads.add(worker);
+          }
+          worker.start();
+          addThread();
+        }
+        else
+          try { Thread.sleep(50); } catch (InterruptedException e) {}
+      }
+    });
+
+    synchronized (threads)
+    {
+      threads.add(runner);
+    }
+    runner.start();
+    addThread();
+  }
+
+  public static long countStepsSimple(double cx, double cy, long maxSteps)
+  {
+    double zx = 0, zy = 0;
+    double temp = 0;
+
+    double zxS = zx * zx, zyS = zy * zy;
+
+    long steps = 0;
+    while (steps < maxSteps && zxS + zyS < 4)
+    {
+      temp = zxS - zyS + cx;
+      zy = sq(zx + zy) - zxS - zyS + cy;
+      zx = temp;
+      steps++;
+      zxS = zx * zx;
+      zyS = zy * zy;
+    }
+    if (zxS + zyS < 4)
+      steps++;
+    return steps == maxSteps + 1 ? -1 : steps;
+  }
+
+  public void clearThreads()
+  {
+    if (getThreads()!=0)
+    {
+      synchronized (threads)
+      {
+        for (Thread t: threads)
+        {
+          t.interrupt();
+          t.stop();
+          remThread();
+        }
+        threads.clear();
+      }
+    }
+  }
+
+  public static double sq(double x)
+  {
+    return x*x;
+  }
+  public static Q q(long n, long d)
+  {
+    return new Q(n, d);
+  }
+
+  public static Q q(long n)
+  {
+    return new Q(n, 1);
+  }
+
+  public static Q trot(int pow)
+  {
+    final Q trot = q(3330,3107);
+    return trot.pow(pow);
+  }
+
+  public static QP qp(Q x, Q y)
+  {
+    return new QP(x, y);
+  }
+
+  public static class HashableView
+  {
+    QP pt;
+    double scl;
+
+    public HashableView(QP point, double scale)
+    {
+      pt = point;
+      scl = scale;
+    }
+
+    public int hashCode()
+    {
+      return new Integer(pt.hashCode() ^ ((Double) scl).hashCode()).hashCode();
+    }
+  }
+
+  public static class Q
+  {
+    BigInteger n, d;
+
+    public Q(BigInteger n, BigInteger d)
+    {
+      this.n = n;
+      this.d = d;
+    }
+
+    public Q(long n, long d)
+    {
+      this.n = new BigInteger(n + "");
+      this.d = new BigInteger(d + "");
+    }
+
+    public Q m(Q o)
+    {
+      return new Q(n.multiply(o.n), d.multiply(o.d));
+    }
+
+    public Q m(int coeff)
+    {
+      return new Q(n.multiply(new BigInteger(coeff + "")), d);
+    }
+
+    public Q a(Q o)
+    {
+      return new Q(n.multiply(o.d).add(o.n.multiply(d)), d.multiply(o.d));
+    }
+
+    public Q s(Q o)
+    {
+      return new Q(n.multiply(o.d).subtract(o.n.multiply(d)), d.multiply(o.d));
+    }
+
+    public int hashCode()
+    {
+      return new Integer(n.hashCode() ^ d.hashCode()).hashCode();
+    }
+
+    public Q pow(int ex)
+    {
+      if (ex == 0)
+        return new Q(1, 1);
+      Q base = this;
+      if (ex < 0)
+      {
+        base = reci();
+        ex = Math.abs(ex);
+      }
+      Q ret = base;
+
+      while (ex-- > 1)
+        ret = ret.m(base);
+      return ret;
+    }
+
+    public Q reci()
+    {
+      return new Q(d, n);
+    }
+
+    @Override
+    public String toString()
+    {
+      return new BigDecimal(n).multiply(new BigDecimal("1000000000")).divide(new BigDecimal(d), BigDecimal.ROUND_HALF_UP).divide(new BigDecimal("1000000000")).toString();
+    }
+  }
+
+  public static class QP
+  {
+    Q x, y;
+
+    public QP(Q x, Q y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+
+    @Override
+    public int hashCode()
+    {
+      return new Integer(x.hashCode() ^ y.hashCode()).hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+      return "{"+x+", "+y+"}";
+    }
+  }
+}
